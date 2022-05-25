49afe0a00fc2b847df708a5aa13d53f7561ca429
Tom Haile <bradford.haile@gmail.com>
2022-05-25 17:14:24 -0500

    remove backward slash from panic, they exist in ledger manifest, not needed in interactions

--- staking-collection/six-stakingcollection-create-machine-account/src/six-stakingcollection-create-machine-account.js
+++ staking-collection/six-stakingcollection-create-machine-account/src/six-stakingcollection-create-machine-account.js
@@ -22,17 +22,17 @@ transaction(nodeID: String, publicKeys: [Crypto.KeyListEntry]) {
 
     prepare(account: AuthAccount) {
         self.stakingCollectionRef = account.borrow<&FlowStakingCollection.StakingCollection>(from: FlowStakingCollection.StakingCollectionStoragePath)
-            ?? panic(\"Could not borrow ref to StakingCollection\")
+            ?? panic("Could not borrow ref to StakingCollection")
 
         if let machineAccount = self.stakingCollectionRef.createMachineAccountForExistingNode(nodeID: nodeID, payer: account) {
             if publicKeys == nil || publicKeys!.length == 0 {
-                panic(\"Cannot provide zero keys for the machine account\")
+                panic("Cannot provide zero keys for the machine account")
             }
             for key in publicKeys {
                 machineAccount.keys.add(publicKey: key.publicKey, hashAlgorithm: key.hashAlgorithm, weight: key.weight)
             }
         } else {
-            panic(\"Could not create a machine account for the node\")
+            panic("Could not create a machine account for the node")
         }
     }
 }
--- staking-collection/six-stakingcollection-register-node/src/six-stakingcollection-register-node.js
+++ staking-collection/six-stakingcollection-register-node/src/six-stakingcollection-register-node.js
@@ -28,7 +28,7 @@ transaction(id: String,
 
     prepare(account: AuthAccount) {
         self.stakingCollectionRef = account.borrow<&FlowStakingCollection.StakingCollection>(from: FlowStakingCollection.StakingCollectionStoragePath)
-            ?? panic(\"Could not borrow ref to StakingCollection\")
+            ?? panic("Could not borrow ref to StakingCollection")
 
         if let machineAccount = self.stakingCollectionRef.registerNode(
             id: id,
@@ -40,7 +40,7 @@ transaction(id: String,
             payer: account) 
         {
             if publicKeys == nil || publicKeys!.length == 0 {
-                panic(\"Cannot provide zero keys for the machine account\")
+                panic("Cannot provide zero keys for the machine account")
             }
             for key in publicKeys! {
                 machineAccount.keys.add(publicKey: key.publicKey, hashAlgorithm: key.hashAlgorithm, weight: key.weight)
