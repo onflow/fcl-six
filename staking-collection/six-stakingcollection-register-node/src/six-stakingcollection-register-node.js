ce992e0c9ac2825dee0b3b6c1a6f5228fdf64b5c
Tom Haile <bradford.haile@gmail.com>
2022-10-28 12:55:53 -0500

    use new enum and translate crypto (go) values to runtime (cadence) enum values. fix interface name for KeyListEntry

--- staking-collection/six-stakingcollection-register-node/src/six-stakingcollection-register-node.js
+++ staking-collection/six-stakingcollection-register-node/src/six-stakingcollection-register-node.js
@@ -1,6 +1,11 @@
 import * as fcl from "@onflow/fcl";
 import { t, config } from "@onflow/fcl";
 
+const PUBLIC_KEY = 0;
+const SIG_ALGO = 1;
+const HASH_ALGO = 2;
+const WEIGHT = 3;
+
 const DEPS = new Set(["0xSTAKINGCOLLECTIONADDRESS"]);
 
 export const TITLE = "Register Node";
@@ -64,67 +69,108 @@ const addressCheck = async (address) => {
     throw new UndefinedConfigurationError(address);
 };
 
-export const template = async ({
-  proposer,
-  authorization,
-  payer,
-  nodeID = "",
-  nodeRole = "",
-  networkingAddress = "",
-  networkingKey = "",
-  stakingKey = "",
-  stakingKeyPop = "",
-  amount = "",
-  publicKeys = [],
-}) => {
-  for (let addr of DEPS) await addressCheck(addr);
 
-  return fcl.pipe([
-    fcl.transaction(CODE),
-    fcl.args([
-      fcl.arg(nodeID, t.String),
-      fcl.arg(nodeRole, t.UInt8),
-      fcl.arg(networkingAddress, t.String),
-      fcl.arg(networkingKey, t.String),
-      fcl.arg(stakingKey, t.String),
-      fcl.arg(stakingKeyPop, t.String),
-      fcl.arg(amount, t.UFix64),
-      fcl.arg(
-        publicKeys.map((pk) => ({
-          fields: [
-            { name: "keyIndex", value: pk.keyIndex },
-            {
-              name: "publicKey",
-              value: {
-                publicKey: pk.publicKey.publicKey,
-                signatureAlgorithm: pk.publicKey.signatureAlgorithm,
-              },
+const cryptoToRuntimeSigningAlgorithm = (cryptoSigningAlgorithm) => {
+    switch (cryptoSigningAlgorithm) {
+        case 1: // crypto.BLSBLS12381
+            return 3; // BLS_BLS12381
+        case 2: // crypto.ECDSAP256
+            return 1; // ECDSA_P256
+        case 3: // crypto.ECDSASecp256k1
+            return 2; // ECDSA_secp256k1
+        default:
+            return 0; // UNKNOWN
+    }
+}
+
+export const template = async ({ proposer, authorization, payer, nodeID = "", nodeRole = "", networkingAddress = "", networkingKey = "", stakingKey = "", stakingKeyPop = "", amount = "", publicKeys = [] }) => {
+    for (let addr of DEPS) await addressCheck(addr)
+
+    const decodedPublicKeys = (publicKeys || []).map(pk => {
+        const values = decode(`0x${pk}`)
+        return {
+            keyIndex: 0,
+            publicKey: {
+                publicKey: [...values[PUBLIC_KEY].map(v => v)],
+                signatureAlgorithm: cryptoToRuntimeSigningAlgorithm(parseInt(values[SIG_ALGO]?.toString("hex"), 16))
             },
-            { name: "hashAlgorithm", value: pk.hashAlgorithm },
-            { name: "weight", value: pk.weight },
-            { name: "isRevoked", value: pk.isRevoked },
-          ],
-        })),
-        t.Optional(
-          t.Array(
-            t.Struct("I.Crypto.KeyListEntry", [
-              { value: t.Int },
-              {
-                value: t.Struct("I.Crypto.PublicKey", [
-                  { value: t.Array(t.UInt8) },
-                  { value: t.UInt8 },
-                ]),
-              },
-              { value: t.UInt8 },
-              { value: t.UFix64 },
-              { value: t.Bool },
-            ])
-          )
-        )
-      ),
-    ]),
-    fcl.proposer(proposer),
-    fcl.authorizations([authorization]),
-    fcl.payer(payer),
-  ]);
+            hashAlgorithm: parseInt(values[HASH_ALGO]?.toString("hex"), 16),
+            weight: `${parseInt(values[WEIGHT]?.toString("hex"), 16) + ".0"}`,
+            isRevoked: false
+        }
+
+    });
+
+    return fcl.pipe([
+        fcl.transaction(CODE),
+        fcl.args([
+            fcl.arg(nodeID, t.String),
+            fcl.arg(nodeRole, t.UInt8),
+            fcl.arg(networkingAddress, t.String),
+            fcl.arg(networkingKey, t.String),
+            fcl.arg(stakingKey, t.String),
+            fcl.arg(stakingKeyPop, t.String),
+            fcl.arg(amount, t.UFix64),
+            fcl.arg(
+                decodedPublicKeys.map((pk) => ({
+                    fields: [
+                        { name: "keyIndex", value: pk.keyIndex },
+                        {
+                            name: "publicKey",
+                            value: {
+                                fields: [
+                                    { name: "publicKey", value: pk.publicKey.publicKey },
+                                    {
+                                        name: "signatureAlgorithm",
+                                        value: {
+                                            fields: [
+                                                { name: "rawValue", value: pk.publicKey.signatureAlgorithm }
+                                            ]
+                                        }
+                                    }
+                                ]
+                            },
+                        },
+                        {
+                            name: "hashAlgorithm",
+                            value: {
+                                fields: [
+                                    { name: "rawValue", value: pk.hashAlgorithm }
+                                ]
+                            },
+                        },
+                        { name: "weight", value: pk.weight },
+                        { name: "isRevoked", value: pk.isRevoked },
+                    ],
+                })),
+                t.Optional(
+                    t.Array(
+                        t.Struct("I.Crypto.Crypto.KeyListEntry", [
+                            { value: t.Int },
+                            {
+                                value: t.Struct("PublicKey", [
+                                    { value: t.Array(t.UInt8) },
+                                    {
+                                        value: t.Enum("SignatureAlgorithm", [
+                                            { value: t.UInt8 }
+                                        ])
+                                    },
+                                ]),
+                            },
+                            {
+                                value: t.Enum("HashAlgorithm", [
+                                    { value: t.UInt8 }
+                                ])
+                            },
+                            { value: t.UFix64 },
+                            { value: t.Bool },
+                        ])
+                    )
+                )
+            ),
+        ]),
+        fcl.proposer(proposer),
+        fcl.authorizations([authorization]),
+        fcl.payer(payer),
+    ]);
 };
