81b4376e79695a147f48165a46027e048862e73a
Tom Haile <bradford.haile@gmail.com>
2024-10-17 15:41:58 -0500

    remove key hash was same as add key, needed to update

--- six-remove-key/src/six-remove-key.js
+++ six-remove-key/src/six-remove-key.js
@@ -3,8 +3,8 @@ import * as t from "@onflow/types"
 
 export const TITLE = "Remove Key"
 export const DESCRIPTION = "Remove key from Account on Flow."
-export const VERSION = "0.2.2"
-export const HASH = "21d4e87df171ccbe015efe69dc2ffd24814c5fc0f2e364daf5c80515ce4a8bd9"
+export const VERSION = "0.2.3"
+export const HASH = "6c7ab72837fdce77a910f6fc0c622c6c4d5b17f6fbf7295f345d50d3508dd515"
 export const CODE = `transaction(keyIndex: Int) {
 	prepare(signer: auth(RevokeKey) &Account) {
 		if let key = signer.keys.get(keyIndex: keyIndex) {
